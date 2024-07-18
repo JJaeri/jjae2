c73da6a499526368aaa36365447d1f2714b1a42a
JJaeri <121221483+JJaeri@users.noreply.github.com>
2024-07-19 00:58:17 +0900

    Update loadHeader.js

--- public/src/loadHeader.js
+++ public/src/loadHeader.js
@@ -1,6 +1,6 @@
 // public/src/loadHeader.js
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("../common/header.html")
+    fetch("/jjae2/public/common/header.html")
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
